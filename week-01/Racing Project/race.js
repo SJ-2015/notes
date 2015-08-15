8c025c16611a3b20942c5ab28df6747669f01100
Sharon Ji <you@example.com>
2015-08-15 16:58:37 -0700

    lay out tracks and playerSquareClass

---
+++ week-01/Racing Project/race.js
@@ -0,0 +1,29 @@
+/*image of horse
+http://www.clipartbest.com/cliparts/dir/M4q/dirM4q4i9.jpeg
+
+Image of car:
+http://media.caranddriver.com/images/media/51/dissected-lotus-based-infiniti-emerg-e-sports-car-concept-top-image-photo-451994-s-original.jpg
+
+
+// Nick's useful methods
+
+e.which
+
+$(seletor).eq.(current cell).text('') --> remove all
+$(seletor).eq.(current cell).text('x') --> put it there all
+diviing each cells
+
+*/
+
+
+
+$(document).ready(function() {
+
+function initialzeRow() {
+	var cell = $
+}
+
+
+
+
+})
