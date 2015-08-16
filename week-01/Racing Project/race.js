90fcf7e3049abf24291afc2d2b8033e74fdcfd27
Sharon Ji <you@example.com>
2015-08-15 18:42:45 -0700

    method for toggle css added

--- week-01/Racing Project/race.js
+++ week-01/Racing Project/race.js
@@ -5,14 +5,18 @@ Image of car:
 http://media.caranddriver.com/images/media/51/dissected-lotus-based-infiniti-emerg-e-sports-car-concept-top-image-photo-451994-s-original.jpg
 
 
-// Nick's useful methods
+1 function moveForward //remove currentsquare, put green on next one
+$(".player1Border:eq(0)").removeClass("playerSquare")
+$(".player1Border:eq(1)").addClass("playerSquare")
 
-e.which
+while (steps less than track total) {
+if pressing F (for player 1), or press L for player 2, then execute below
+var step = 0;
+$(".player1Border:eq(step)").removeClass("playerSquare")
+$(".player1Border:eq(step+1)").addClass("playerSquare")
+i++;
 
-$(seletor).eq.(current cell).text('') --> remove all
-$(seletor).eq.(current cell).text('x') --> put it there all
-diviing each cells
-
+}
 */
 
 
