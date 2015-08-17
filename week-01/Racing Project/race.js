4dc651d0d843ab3b8cff193335cc69f1aba3e9d2
Sharon Ji <you@example.com>
2015-08-16 23:43:27 -0700

    both able to move, with instructions

--- week-01/Racing Project/race.js
+++ week-01/Racing Project/race.js
@@ -1,41 +1,37 @@
-/*image of horse
-http://www.clipartbest.com/cliparts/dir/M4q/dirM4q4i9.jpeg
-Image of car:
-http://media.caranddriver.com/images/media/51/dissected-lotus-based-infiniti-emerg-e-sports-car-concept-top-image-photo-451994-s-original.jpg
-fff1 function moveForward //remove currentsquare, put green on next one
-
+/*
 while (either Player1 nor Player2 Win) {
-if pressing F (for player 1), or press L for player 2, then execute below
-var step = 0;
-$(".player1Border:eq(step)").removeClass("playerSquare")
-$(".player1Border:eq(step+1)").addClass("playerSquare")
-step++;
-http://keycode.info/    ==> keycode info (f=70; l=76)
-}
-/*function CheckNoWinner() 
-var winner;
-return no winner = true; 
 */
 console.log("Sanity Check: JS is working!");
+var playerOneLocation =0;
+var playerTwoLocation =0;
 
 $(document).ready(function() {
-var step=0;
+	$(document).on('keypress',function(e){  
+		var key =e.which;
+		console.log(key);
 
-$(document).on('keypress',function(e){  
-var key =e.which;
-console.log(key);
-//set while no winner loop later
-	if (key===102) {              //f = 102, if f is pressed
-		console.log(e.which);
-		$(".player1Border:eq(0)").removeClass("playerSquare");
-		$(".player1Border:eq(1)").addClass("playerSquare");
+		//set while no winner loop later
+		if (key===102) {              //f = 102, if f is pressed
+			console.log(e.which);
+			$(".player1Border:eq("+playerOneLocation+")").removeClass("playerSquare");
+			$(".player1Border:eq("+(playerOneLocation+1)+")").addClass("playerSquare");
+			playerOneLocation=playerOneLocation+1;
+			console.log(playerOneLocation);
+		}
+		if (key===108) {              //l = 108, if f is pressed
+			console.log(e.which);
+			$(".player2Border:eq("+playerTwoLocation+")").removeClass("playerSquare");
+			$(".player2Border:eq("+(playerTwoLocation+1)+")").addClass("playerSquare");
+			playerTwoLocation=playerTwoLocation+1;
+			console.log(playerTwoLocation);
+		}
+	})
+})	
+
+
+function checkWinner(playerLocation) {
+//
+
+
+}
 	
-	}
-
-
-})
-
-
-
-
-})
