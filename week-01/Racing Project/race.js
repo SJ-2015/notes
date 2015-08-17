a21d35ce2146fb1f781f0dc98d537f143deb2ac4
Sharon Ji <you@example.com>
2015-08-17 00:37:30 -0700

    game works! overflows but ok

--- week-01/Racing Project/race.js
+++ week-01/Racing Project/race.js
@@ -1,37 +1,44 @@
-/*
-while (either Player1 nor Player2 Win) {
-*/
+
 console.log("Sanity Check: JS is working!");
 var playerOneLocation =0;
 var playerTwoLocation =0;
+var trackLength =8;
+var winner; 
 
 $(document).ready(function() {
-	$(document).on('keypress',function(e){  
-		var key =e.which;
-		console.log(key);
 
-		//set while no winner loop later
-		if (key===102) {              //f = 102, if f is pressed
-			console.log(e.which);
-			$(".player1Border:eq("+playerOneLocation+")").removeClass("playerSquare");
-			$(".player1Border:eq("+(playerOneLocation+1)+")").addClass("playerSquare");
-			playerOneLocation=playerOneLocation+1;
+		$(document).on('keypress',function(e){  
+			var key =e.which;
+
+			if (key===102) {              //f = 102, if f is pressed
+				$(".player1Border:eq("+playerOneLocation+")").removeClass("playerSquare");
+				$(".player1Border:eq("+(playerOneLocation+1)+")").addClass("playerSquare");
+				playerOneLocation=playerOneLocation+1;
+			}
+			if (key===108) {              //l = 108, sif f is pressed
+				$(".player2Border:eq("+playerTwoLocation+")").removeClass("playerSquare");
+				$(".player2Border:eq("+(playerTwoLocation+1)+")").addClass("playerSquare");
+				playerTwoLocation=playerTwoLocation+1;
+				
+			}
 			console.log(playerOneLocation);
-		}
-		if (key===108) {              //l = 108, if f is pressed
-			console.log(e.which);
-			$(".player2Border:eq("+playerTwoLocation+")").removeClass("playerSquare");
-			$(".player2Border:eq("+(playerTwoLocation+1)+")").addClass("playerSquare");
-			playerTwoLocation=playerTwoLocation+1;
 			console.log(playerTwoLocation);
+			if ((playerOneLocation === trackLength) || (playerTwoLocation === trackLength)) {
+			announceWinner(playerOneLocation,playerTwoLocation,trackLength);
 		}
-	})
-})	
+		})
+})
+	
+function announceWinner(playerOneLocation,playerTwoLocation,trackLength) {
+	if (playerOneLocation===trackLength) {
+		winner = "Player1";
+	} else if (playerTwoLocation===trackLength) {
+		winner = "player2";
+	}
 
-
-function checkWinner(playerLocation) {
-//
-
-
+	alert(winner + " won the game!");
+	return winner;
 }
+
+
 	
